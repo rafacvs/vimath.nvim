d8c3d6e011259d3d42ce3b585d2d2396eb1af525
rafacvs <rafa.cvs1@hotmail.com>
2025-09-22 15:37:33 -0300

    fix: correctly saving/reading files

--- lua/vimath.lua
+++ lua/vimath.lua
@@ -17,12 +17,13 @@ function M.get_current_buffer_content()
 	local PLUGIN_PATH = vim.fn.expand("LOCAL_DEV_DIR_DO_NOT_COMMIT")
 	local FILES_PATH = PLUGIN_PATH .. "tmp/"
 	local FILE_NAME = "test.txt"
+	local CURR_FILE_NAME = FILES_PATH .. FILE_NAME
 
 	-- Ensure dir
 	vim.fn.mkdir(FILES_PATH, "p")
-	local file = io.open(FILES_PATH .. FILE_NAME, "w")
+	local file = io.open(CURR_FILE_NAME, "w")
 	if not file then
-		vim.notify("Could not open file: " .. FILES_PATH .. FILE_NAME, vim.log.levels.ERROR)
+		vim.notify("Could not open file: " .. CURR_FILE_NAME, vim.log.levels.ERROR)
 		return
 	end
 
@@ -31,13 +32,14 @@ function M.get_current_buffer_content()
 	end
 	file:close()
 
-	local cmd = "cd " .. PLUGIN_PATH .. " && go run core/*.go --file " .. FILES_PATH .. FILE_NAME
+	local cmd = "cd " .. PLUGIN_PATH .. " && go run core/*.go --file tmp/" .. FILE_NAME
 	local output = vim.fn.system(cmd)
 	print(output)
 
-	file = io.open(FILES_PATH .. "OUTPUT_" .. FILE_NAME, "r")
+	CURR_FILE_NAME = FILES_PATH .. "OUTPUT_" .. FILE_NAME
+	file = io.open(CURR_FILE_NAME, "w")
 	if not file then
-		vim.notify("Could not open file: " .. FILES_PATH .. FILE_NAME, vim.log.levels.ERROR)
+		vim.notify("Could not open file: " .. CURR_FILE_NAME, vim.log.levels.ERROR)
 		return
 	end
 	file:write(output)
